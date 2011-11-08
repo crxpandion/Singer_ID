b46908b72d182ad3f86442231be7cf41ff0867f6
Kai Hayashi <k.hermitian@gmail.com>
2011-11-07 23:51:51 -0600

    .gitignore and initial process function stub

---
+++ ProcessSample.m
@@ -0,0 +1,9 @@
+function [feature_vector] = ProcessSample(sample)
+% This function needs to do a couple of things first off it needs to
+% caculate the instantaneous frequency and amplitude. Then it needs to
+% perform the CTF estimation based off of the approach suggested in B & W.
+% We should be able to use the frequency estimators in
+% http://tftb.nongnu.org/
+% To determine our ifreq and iamp
+
+end
