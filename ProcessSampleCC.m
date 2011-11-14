406f20ce4773372746b92a3d1652f2db1e282871
Kai Hayashi <k.hermitian@gmail.com>
2011-11-14 17:37:32 -0600

    updated file names

---
+++ ProcessSampleCC.m
@@ -0,0 +1,15 @@
+function [feature_vector] = ProcessSample(sample)
+% This function needs to do a couple of things.
+% First off it needs to calculate the instantaneous frequency and
+% amplitude. Then it needs to perform the CTF estimation based off of the 
+% approach suggested in B & W.
+% We should be able to use the frequency estimators in
+% http://tftb.nongnu.org/
+% To determine our ifreq and iamp
+%
+% ccepstral coefficients vs formants
+%
+
+
+
+end
