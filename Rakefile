6109764a5c69f41c642b55c86844b9f8b2593272
Cary Lee <carylee@gmail.com>
2011-12-07 15:53:52 -0600

    Added a whole bunch to the website

---
+++ Rakefile
@@ -0,0 +1,4 @@
+task :default do
+  `haml index.haml > index.html`
+  `haml dataset.haml > dataset.html`
+end
--- js/script.js
+++ js/script.js
@@ -2,25 +2,14 @@
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  //$(".topbar").scrollSpy();
+  $("a[rel=popover]")
+  .popover({
+    offset: 10
+  })
+  $("a[rel=twipsy]")
+  .twipsy({
+    offset: 10
+  })
+});
